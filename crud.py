b81cfd7a523be66188a00e094ae914ca39653ab9
Leah Ewing <leahewing@live.com>
2021-05-27 22:02:37 +0000

    Final commit for 5/27

--- crud.py
+++ crud.py
@@ -1,6 +1,6 @@
 """CRUD operations."""
 
-from model import db, User, Movie, Rating, connect_to_db
+from model import db, User, Movie, Ratings, connect_to_db
 
 # Functions start here!
 
@@ -18,11 +18,10 @@ def create_user(email, password):
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
 
-    movie = Movie(movie_id = movie_id,
-                    title = title,
-                    overview = overview,
-                    release_date = release_date,
-                    poster_path = poster_path)
+    movie = Movie(title = title,
+                overview = overview,
+                release_date = release_date,
+                poster_path = poster_path)
 
     db.session.add(movie)
     db.session.commit()
@@ -33,7 +32,7 @@ def create_movie(title, overview, release_date, poster_path):
 def create_rating(user, movie, score):
     """Create and return a new rating."""
 
-    rating = Rating(user=user, movie=movie, score=score)
+    rating = Ratings(user=user, movie=movie, score=score)
 
     db.session.add(rating)
     db.session.commit()
